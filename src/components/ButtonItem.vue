5adcfd2a483f4a2ceea431b7f23294aec9cfce3d
WhiteFangs <white-fangs@hotmail.fr>
2024-12-30 13:08:59 +0100

    Add button for voting

---
+++ src/components/ButtonItem.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  emits: ['click'],
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+  },
+});
+</script>
+
+<template>
+  <button class="button" @click="$emit('click')">
+    {{ label }}
+  </button>
+</template>
+
+<style scoped>
+.button {
+  border: .3em black solid;
+  border-radius: .3em;
+  background: black;
+  color: white;
+  cursor: pointer;
+  padding: .3em .5em;
+}
+
+.button:hover {
+  color: black;
+  background: white;
+}
+</style>
--- src/components/CandidateItem.vue
+++ src/components/CandidateItem.vue
@@ -1,8 +1,13 @@
 <script lang="ts">
 import type { Cat } from '@/interfaces/cats';
 import { defineComponent, type PropType } from 'vue';
+import ButtonItem from './ButtonItem.vue';
 
 export default defineComponent({
+  emits: ['vote'],
+  components: {
+    ButtonItem
+  },
   props: {
     cat: {
       type: Object as PropType<Cat>,
@@ -14,12 +19,24 @@ export default defineComponent({
 
 <template>
   <div class="candidate">
-    <img :src="cat.url" :alt="`ID : ${cat.id}`" :title="`ID : ${cat.id}`">
+    <div>
+      <img :src="cat.url" :alt="`ID : ${cat.id}`" :title="`ID : ${cat.id}`" @click="$emit('vote')">
+    </div>
+    <div>
+      <ButtonItem label="J'aime" @click="$emit('vote')" />
+    </div>
   </div>
 </template>
 
 <style scoped>
+.candidate {
+  display: flex;
+  flex-direction: column;
+}
+
 .candidate img {
   max-height: 40vh;
+  border-radius: 1em;
+  cursor: pointer;
 }
 </style>
